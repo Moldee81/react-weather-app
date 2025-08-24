3b94d5bd2b89ca7a87e754c81bb90853db3c4995
Moldee81 <a.penatomczak@gmail.com>
2025-08-24 06:21:08 +0100

    Initial commit,

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>React weather app </h1>
+      <footer>
+        This project was coded by
+        <span className="accent"> Anita Peña-Tomczak</span> and it's
+        open-sourced
+        <a href="" target="_blank" rel="noopener noreferrer">
+          {" "}
+          on GitHub.
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
