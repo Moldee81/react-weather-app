64aa26cb6ff15f0ea7e6669ab0447399d04ec680
Moldee81 <a.penatomczak@gmail.com>
2025-08-25 10:41:44 +0100

    Added a bootstrap and Weather component-basic html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>React weather app </h1>
-      <footer>
-        This project was coded by
-        <span className="accent"> Anita Peña-Tomczak</span> and it's
-        open-sourced
-        <a href="" target="_blank" rel="noopener noreferrer">
-          {" "}
-          on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by
+          <span className="accent"> Anita Peña-Tomczak</span> and it's
+          open-sourced
+          <a
+            href="https://github.com/Moldee81/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,39 @@
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Monday 09:51</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="" alt="mostly cloudy" /> 6
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
