07858b37f8ccc2b8daad9688a1c89678a4328d95
Moldee81 <a.penatomczak@gmail.com>
2025-09-03 10:11:18 +0100

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,18 +4,18 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="">
+    <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li class="text-capitalize">{props.data.condition}</li>
+        <li className="text-capitalize">{props.data.condition}</li>
       </ul>
       <div className="row">
-        <div className="col-6">
+        <div className="col-6 d-flex">
           <img src={props.data.iconUrl} alt={props.data.icon}></img>
-          <WeatherTemperature celsius={props.data.temperature} />
+          <WeatherTemperature celsius={props.data.temperature.current} />
         </div>
         <div className="col-6 weather-extras">
           <ul>
