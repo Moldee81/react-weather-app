986a13214e6c025b74f0e64fa8bedee842cd0d69
Moldee81 <a.penatomczak@gmail.com>
2025-09-12 13:35:55 +0100

    Removing Fahren heit conversion

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,36 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Forecast.css";
 import Axios from "axios";
 import ForecastDay from "./ForecastDay";
 
-export default function Forecast() {
+export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(false);
+  let [forecast, setForecast] = useState([]);
+
+  useEffect(() => {
+    setLoaded(false);
+    let city = props.city;
+    const url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=6dod2fbfa8c43fe552ftae49bc36d90b`;
+    Axios.get(url)
+      .then(handleResponse)
+      .catch((error) => {
+        console.error("Error fetching weather data: ", error);
+        setLoaded(true); // Set loaded to true even if there's an error
+      });
+  }, [props.city]);
+
   function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
+    console.log(response); // Log the response for debugging
+    if (response.data && Array.isArray(response.data.daily)) {
+      setForecast(response.data.daily); // Set forecast data
+    } else {
+      console.error("Unexpected data structure", response.data);
+      setForecast([]); // Ensure forecast is set to an empty array in case of unexpected data
+    }
+    setLoaded(true); // Data has been loaded
   }
-  if (loaded) {
-    console.log(forecast);
-    return (
-      <div className="Forecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 4) {
-              return (
-                <div className="col" key={index}>
-                  <ForecastDay data={dailyForecast} />
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
+
+  // Show loading message until data is loaded
+  if (!loaded) {
+    return <div>Loading...</div>;
+  }
+
+  // Render the forecast data
+  console.log(forecast); // Log the forecast for debugging
+  return (
+    <div className="Forecast">
+      <div className="row">
+        {forecast.slice(0, 4).map((dailyForecast, index) => (
+          <div className="col" key={index}>
+            <ForecastDay data={dailyForecast} />
+          </div>
+        ))}
       </div>
-    );
-  } else {
-    const url = `https://api.shecodes.io/weather/v1/forecast?query="Poznan"&key=6dod2fbfa8c43fe552ftae49bc36d90b`;
-    Axios.get(url).then(handleResponse);
-    return null;
-  }
+    </div>
+  );
 }
